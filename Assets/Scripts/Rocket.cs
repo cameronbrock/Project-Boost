7fb7a13c6f0e1935f3146d5e0956fe32ccc41d8c
Cameron Brock <cameronbrock33@gmail.com>
2019-08-11 18:57:53 -0500

    Added ship, moveable in one direction.

---
+++ Assets/Scripts/Rocket.cs
@@ -0,0 +1,41 @@
+﻿//using System.Collections;
+//using System.Collections.Generic;
+//using System;
+using UnityEngine;
+
+public class Rocket : MonoBehaviour
+{
+
+    Rigidbody rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ProcessInput();
+    }
+
+    private void ProcessInput()
+    {
+        // Thrust
+        if (Input.GetKey(KeyCode.W))
+        {
+            rb.AddRelativeForce(Vector3.up);
+        }
+
+        // Rotation
+        if ( Input.GetKey(KeyCode.A) && !Input.GetKey(KeyCode.D) )
+        {
+            print("Rotating left.");
+        }
+        else if ( Input.GetKey(KeyCode.D) && !Input.GetKey(KeyCode.A) )
+        {
+            print("Rotating right.");
+        }
+    }
+}
