760c5c9644e8c81f81dd4b24eff5257d38e6e79c
Cameron Brock <cameronbrock33@gmail.com>
2019-08-14 03:02:18 -0500

    Implemented particle effects.

---
+++ Assets/Scripts/Oscillator.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[DisallowMultipleComponent]
+public class Oscillator : MonoBehaviour
+{
+
+    [SerializeField] Vector3 movementVector;
+    [SerializeField] float period = 2f;
+
+    [Range(0, 1)]
+    [SerializeField]
+    float movementFactor; // 0 for not moved, 1 for fully moved.
+
+    Vector3 startingPosition;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        startingPosition = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        // Set a movement factor.
+        if (period <= Mathf.Epsilon) return;
+        float cycles = Time.time / period;
+
+        const float tau = 2 * Mathf.PI;
+        float rawSinWave = Mathf.Sin(cycles * tau);
+
+        //print(rawSinWave);
+
+        movementFactor = (rawSinWave / 2f) + 0.5f;
+
+        Vector3 offset = movementFactor * movementVector;
+        transform.position = startingPosition + offset;
+    }
+}
--- Assets/Scripts/Rocket.cs
+++ Assets/Scripts/Rocket.cs
@@ -10,8 +10,28 @@ public class Rocket : MonoBehaviour
 
     int currentLevel;
 
+    // Rocket specifications.
     [SerializeField] float thrustForce;
     [SerializeField] float rotationSpeed;
+
+    [SerializeField] float levelLoadDelay = 2.0f;
+
+    // Audio clips.
+    [SerializeField] AudioClip mainEngine;
+    [SerializeField] AudioClip deathSound;
+    [SerializeField] AudioClip successSound;
+
+    // Particle effects.
+    [SerializeField] ParticleSystem engineParticles;
+    [SerializeField] ParticleSystem deathParticles;
+    [SerializeField] ParticleSystem successParticles;
+
+    enum State
+    {
+        ALIVE, DYING, TRANSCENDING
+    }
+
+    [SerializeField] State state = State.ALIVE;
 
     // Start is called before the first frame update
     void Start()
@@ -30,31 +50,65 @@ public class Rocket : MonoBehaviour
         // Prevent the rocket from tipping over by restricting rotation about X and Y.
         //rb.constraints = RigidbodyConstraints.FreezeRotationX | RigidbodyConstraints.FreezeRotationY;
 
+        //Invoke("PlayLoadSound", 0.0f);
+
+    }
+
+    private void PlayLoadSound()
+    {
+        //audio.PlayOneShot(levelLoad);
     }
 
     // Update is called once per frame
+    
     void Update()
     {
         ProcessInput();
     }
+    
+    // Reset the current level.
+    private void SameLevel()
+    {
+        SceneManager.LoadScene(currentLevel);
+        state = State.ALIVE;
+    }
+
+    // Load the next scene/level.
+    private void NextLevel()
+    {
+        currentLevel++;
+        SceneManager.LoadScene(currentLevel);
+        state = State.ALIVE;
+    }
 
     private void OnCollisionEnter(Collision collision)
     {
+        // If we are not alive, ignore collisions.
+        if (state != State.ALIVE)
+        {
+            return;
+        }
+
+        // Process collision.
         switch (collision.gameObject.tag)
         {
             case "Friendly":
-                print("OK");
                 break;
             case "Goal":
-                currentLevel++;
-                print("GOAL - Current level: " + currentLevel);
-                SceneManager.LoadScene(currentLevel);
+                state = State.TRANSCENDING;
+                audio.Stop();
+                audio.PlayOneShot(successSound);
+                successParticles.Play();
+                Invoke("NextLevel", levelLoadDelay);
                 break;
             case "Fuel":
                 break;
             default:
-                print("DEAD - Current level: " + currentLevel);
-                SceneManager.LoadScene(currentLevel);
+                state = State.DYING;
+                audio.Stop();
+                audio.PlayOneShot(deathSound);
+                deathParticles.Play();
+                Invoke("SameLevel", levelLoadDelay);
                 break;
         }
     }
@@ -62,27 +116,32 @@ public class Rocket : MonoBehaviour
     // Thrust
     private void ProcessThrustInput()
     {
+        
         // Forward thrust
+        
         if (Input.GetKey(KeyCode.W))
         {
             rb.AddRelativeForce(thrustForce * Time.deltaTime * Vector3.up);
 
             if (!audio.isPlaying)
             {
-                audio.Play();
+                audio.PlayOneShot(mainEngine);
             }
-
+            engineParticles.Play();
         }
+        
         else
         {
             audio.Stop();
+            engineParticles.Stop();
         }
-
+        
         // Backwards thrust
         if (Input.GetKey(KeyCode.S))
         {
             rb.AddRelativeForce(thrustForce * Time.deltaTime * Vector3.down);
         }
+        
     }
 
     // Rotation
@@ -109,18 +168,30 @@ public class Rocket : MonoBehaviour
     {
 
         transform.position = new Vector3(transform.position.x, transform.position.y, 0);
+        
+        if (state == State.ALIVE)
+        {
+            //Thrust
+            ProcessThrustInput();
 
-        //Thrust
-        ProcessThrustInput();
-
-        // Rotation
-        ProcessRotationInput();
-
+            // Rotation
+            ProcessRotationInput();
+        }
+        else
+        {
+            //audio.Stop();
+            rb.constraints = RigidbodyConstraints.None;
+            //rb.freezeRotation = true;
+            //rb.constraints = RigidbodyConstraints.FreezePositionX
+            //               | RigidbodyConstraints.FreezePositionY
+            //               | RigidbodyConstraints.FreezePositionZ;
+        }
+        
         // Allow the player to reset position.
         if (Input.GetKey(KeyCode.R))
         {
             SceneManager.LoadScene(currentLevel);
         }
-
+        
     }
 }
